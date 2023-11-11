825d2e6acada20043e04759c9ac75506f6cafa00
Yusuf Arslan <yusufarsln98@outlook.com>
2023-11-11 16:31:46 +0300

    Admin route added.

--- web/src/Routes.jsx
+++ web/src/Routes.jsx
@@ -12,6 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/admin" page={AdminPage} name="admin" />
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
