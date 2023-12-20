e36ac17c93c22a3ae1d2a0d8ff8cb7d8abd9068e
Yusuf Arslan <yusufarsln98@outlook.com>
2023-12-21 01:44:53 +0300

    feat: prisma generate

---
+++ api/db/migrations/20231220222959_migration/migration.sql
@@ -0,0 +1,149 @@
+-- CreateEnum
+CREATE TYPE "Language" AS ENUM ('English', 'French', 'Turkish', 'Spanish', 'German', 'Italian', 'Portuguese', 'Japanese');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'PREMIUM_USER', 'USER');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE', 'RATHER_NOT_SAY', 'COSTUM');
+
+-- CreateEnum
+CREATE TYPE "Theme" AS ENUM ('LIGHT', 'DARK');
+
+-- CreateTable
+CREATE TABLE "Avatar" (
+    "id" SERIAL NOT NULL,
+    "data" TEXT NOT NULL,
+
+    CONSTRAINT "Avatar_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" TIMESTAMP(3),
+    "roles" "Role"[] DEFAULT ARRAY['USER']::"Role"[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "daysStudied" TIMESTAMP(3)[],
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserConfig" (
+    "id" SERIAL NOT NULL,
+    "avatarId" INTEGER,
+    "defaultAvatarIndex" INTEGER NOT NULL DEFAULT 0,
+    "birthday" TIMESTAMP(3),
+    "gender" "Gender",
+    "theme" "Theme" NOT NULL DEFAULT 'LIGHT',
+    "languageNative" "Language" NOT NULL,
+    "languageLearning" "Language" NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "UserConfig_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Set" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "termsLanguage" "Language" NOT NULL,
+    "translationsLanguage" "Language" NOT NULL,
+    "folderId" INTEGER,
+    "userId" INTEGER,
+
+    CONSTRAINT "Set_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FlashCard" (
+    "id" SERIAL NOT NULL,
+    "term" TEXT NOT NULL,
+    "cardId" INTEGER NOT NULL,
+    "boost" DOUBLE PRECISION NOT NULL DEFAULT 1,
+    "setId" INTEGER NOT NULL,
+
+    CONSTRAINT "FlashCard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Word" (
+    "id" SERIAL NOT NULL,
+    "term" TEXT NOT NULL,
+    "termSearch" TEXT NOT NULL,
+    "termsLanguage" "Language" NOT NULL,
+    "translationsLanguage" "Language" NOT NULL,
+
+    CONSTRAINT "Word_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Card" (
+    "id" SERIAL NOT NULL,
+    "translation" TEXT NOT NULL,
+    "definition" TEXT NOT NULL,
+    "example" TEXT NOT NULL,
+    "cefrLevel" TEXT NOT NULL,
+    "partOfSpeech" TEXT NOT NULL,
+    "wordId" INTEGER NOT NULL,
+
+    CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserConfig_userId_key" ON "UserConfig"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Word_termSearch_key" ON "Word"("termSearch");
+
+-- AddForeignKey
+ALTER TABLE "UserConfig" ADD CONSTRAINT "UserConfig_avatarId_fkey" FOREIGN KEY ("avatarId") REFERENCES "Avatar"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserConfig" ADD CONSTRAINT "UserConfig_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Set" ADD CONSTRAINT "Set_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Set" ADD CONSTRAINT "Set_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FlashCard" ADD CONSTRAINT "FlashCard_cardId_fkey" FOREIGN KEY ("cardId") REFERENCES "Card"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FlashCard" ADD CONSTRAINT "FlashCard_setId_fkey" FOREIGN KEY ("setId") REFERENCES "Set"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Card" ADD CONSTRAINT "Card_wordId_fkey" FOREIGN KEY ("wordId") REFERENCES "Word"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
