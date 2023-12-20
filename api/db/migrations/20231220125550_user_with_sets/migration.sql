d5e7cb352abdb642e19495ab8a34da8957b286b6
Yusuf Arslan <yusufarsln98@outlook.com>
2023-12-21 01:29:35 +0300

    feat: delete previous migrations

--- api/db/migrations/20231220125550_user_with_sets/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Set" ADD COLUMN     "userId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Set" ADD CONSTRAINT "Set_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
