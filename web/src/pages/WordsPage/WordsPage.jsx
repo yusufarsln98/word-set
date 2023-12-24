44b70070101b04f09f215316b9f908f9c535f26b
Yusuf Arslan <yusufarsln98@outlook.com>
2023-12-24 13:33:39 +0300

    feature: show meanings in the admin/words page

--- web/src/pages/WordsPage/WordsPage.jsx
+++ web/src/pages/WordsPage/WordsPage.jsx
@@ -1,10 +1,10 @@
 import { MoreOutlined } from '@ant-design/icons'
-import { Button, Table, message } from 'antd'
+import { Button, Space, Table, message } from 'antd'
 
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags, useMutation, useQuery } from '@redwoodjs/web'
 
-import { DELETE_WORD_MUTATION, QUERY_DICTIONARY } from 'src/graphql'
+import { DELETE_WORD_MUTATION, QUERY_DICTIONARY } from 'src/graphql_queries'
 
 const WordsPage = ({ dictionaryId }) => {
   const { data, error, loading } = useQuery(QUERY_DICTIONARY, {
@@ -58,9 +58,25 @@ const WordsPage = ({ dictionaryId }) => {
       <MetaTags title="Words" description="Words page" />
       <Table
         title={() => <h2>{data?.dictionary?.name} Dictionary</h2>}
-        dataSource={data?.dictionary?.words}
+        // set keys to each word then add to the dataSource
+        dataSource={data?.dictionary?.words.map((word) => {
+          return {
+            ...word,
+            key: word.id,
+          }
+        })}
         columns={columns}
         loading={loading}
+        expandable={{
+          expandedRowRender: (record) => (
+            <Space direction="vertical">
+              {record.meanings.map((meaning) => (
+                <p key={meaning.id}>{meaning.definition}</p>
+              ))}
+            </Space>
+          ),
+          rowExpandable: (record) => record?.meanings?.length > 0,
+        }}
         bordered
       />
     </>
